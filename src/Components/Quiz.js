bb0a775a1ee73661d02e42261f8893f91b069cb0
HoangT <ac5217@student.jamk.fi>
2022-03-18 12:45:29 +0200

    update buttons

---
+++ src/Components/Quiz.js
@@ -0,0 +1,50 @@
+import React, { useContext, useState } from "react";
+import { Question } from "../Helpers/QuestionBank";
+import { QuizContext } from "../Helpers/Context";
+
+function Quiz() {
+  const { score, setScore, setGameState } = useContext(QuizContext);
+  const [currQuestion, setCurrQuestion] = useState(0);
+  const [optionChosen, setOptionChosen] = useState("");
+
+  const nextQuestion = () => {
+    if (Question[currQuestion].answer == optionChosen) {
+      setScore(score + 1);
+    }
+    alert(score);
+    setCurrQuestion(currQuestion + 1);
+  };
+
+  const finishQuiz = () => {
+    if (Question[currQuestion].answer == optionChosen) {
+      setScore(score + 1);
+    }
+    setGameState("endScreen");
+  };
+  return (
+    <div className="Quiz">
+      <h2>{Question[currQuestion].prompt}</h2>
+      <div className="Option">
+        <button onClick={() => setOptionChosen("A")}>
+          {Question[currQuestion].optionA}
+        </button>
+        <button onClick={() => setOptionChosen("B")}>
+          {Question[currQuestion].optionB}
+        </button>
+        <button onClick={() => setOptionChosen("C")}>
+          {Question[currQuestion].optionC}
+        </button>
+        <button onClick={() => setOptionChosen("C")}>
+          {Question[currQuestion].optionD}
+        </button>
+      </div>
+      {currQuestion == Question.length - 1 ? (
+        <button onClick={finishQuiz}>Finish Quiz</button>
+      ) : (
+        <button onClick={nextQuestion}>Next Question</button>
+      )}
+    </div>
+  );
+}
+
+export default Quiz;
---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Question = [
+  {
+    prompt: "What is the capital of Finland?",
+    optionA: "Turku",
+    optionB: "Tampere",
+    optionC: "Rovaniemi",
+    optionD: "Helsinki",
+    answer: "D",
+  },
+  {
+    prompt: "When did Finland become independent?",
+    optionA: "11.9.1912",
+    optionB: "6.12.1917",
+    optionC: "19.9.1912",
+    optionD: "12.4.1910",
+    answer: "B",
+  },
+  {
+    prompt: "What is the calling code of the country??",
+    optionA: "+358",
+    optionB: "+891",
+    optionC: "+691",
+    optionD: "+291",
+    answer: "A",
+  },
+];
